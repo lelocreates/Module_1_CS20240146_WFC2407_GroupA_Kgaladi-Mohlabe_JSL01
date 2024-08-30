0e4ead00fe5b50b7d5250013664fd7dec33d1283
Kgaladi Lelo Mohlabe <kgaladipmohlabe@gmail.com>
2024-08-30 04:21:35 +0200

    Add validation logic

--- script.js
+++ script.js
@@ -5,8 +5,17 @@ function validateSyntax() {
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
+        /*petID needs to take "pet_" followed by a variety of numbers and letters 
+        - used a regular expression for this*/
+    let petID = /^pet_\d+[a-zA-Z]+$/; 
+//used .test to check where the input contained the pattern specified
+    if (petID.test(input)) {
+        result = 'Valid Syntax 🟢';
+    } else {
+        result = 'Invalid Syntax 🔴';
+    }
 
-            document.getElementById('result').innerText = result;
+    document.getElementById('result').innerText = result;
 }
 
 
